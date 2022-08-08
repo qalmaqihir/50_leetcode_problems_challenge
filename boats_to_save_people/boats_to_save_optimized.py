3e01a1fe7abe425a6d2db2b2c89a9e47e46af9b1
qalmaqihir <supine.me@gmail.com>
2022-08-08 10:58:42 +0600

    boats to save peope and container withe most water problem solution added

---
+++ boats_to_save_people/boats_to_save_optimized.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+def boats_to_save_people_optimized(people:List[int],limit:int)-> int:
+    left = 0
+    right = len(people)-1
+    boats=0
+    while left<=right:
+        if left==right:
+            boats+=1
+            break
+        if people[left]+people[right]<=limit:
+            left+=1
+        right-=1
+        boats+=1
+    return boats
