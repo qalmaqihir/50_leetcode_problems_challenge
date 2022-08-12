6ec9209f28bc1c847d55ca942b45325eb6b96c27
qalmaqihir <supine.me@gmail.com>
2022-08-12 09:19:39 +0600

    contain duplicates and two sum optimized

--- contain_duplicates/contain_duplicates.py
+++ contain_duplicates/contain_duplicates.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-def contain_duplicates(nums:List[int])-> bool:
+def contain_duplicates_naive(nums:List[int])-> bool:
     for i in range(len(nums)):
         for j in range(len(nums)):
             if i==j:
@@ -10,6 +10,23 @@ def contain_duplicates(nums:List[int])-> bool:
                 return True
     return False
 
+def contain_duplicates_optimzied(nums:List[int])-> bool:
+    check_dict={}
+    print(f"Dictionary at start = {check_dict}")
+    for i in range(len(nums)):
+        if nums[i] in check_dict:
+            print(f"nums[i] {nums[i]} is in dictionary at {i}")
+            return True
+        check_dict[nums[i]]=False
+        print(check_dict)
+
+
+
+
+
+
+
 
 nums=[1,2,3,1]
-print(contain_duplicates(nums))
+# print(contain_duplicates_naive(nums:List[int])-> bool:)
+print(contain_duplicates_optimzied(nums))
